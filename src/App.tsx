a79e32eccc001a19845ec7aede67c05352764d28
yuanxinxin <yuanxinxin@111.com.cn>
2021-01-22 17:06:49 +0800

    '高阶组件'

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
 //App.js
 import React from 'react';
-import Son from './son';//引入子组件
 // 创建一个 theme Context,
-export const { Provider, Consumer } = React.createContext("默认名称");
 export default class App extends React.Component {
   render() {
-    let name = "小人头"
     return (
-      //Provider共享容器 接收一个name属性
-      <Provider value={name}>
-        <div style={{ border: '1px solid red', width: '30%', margin: '50px auto', textAlign: 'center' }}>
-          <p>父组件定义的值:{name}</p>
-          <Son />
-        </div>
-      </Provider>
+      <div>app</div>
     );
   }
 }
